d332d77bdbee89efef66c5cfbcd06353c370df91
Jeffrey Perrone <jperrone@well.com>
2014-04-15 16:29:23 -0700

    Part way through bagger.

--- lab/lib/mathexpert.rb
+++ lab/lib/mathexpert.rb
@@ -5,16 +5,55 @@ module MathExpert
   #5! = 5 *  4 *  3 *  2 *  1 = 120
   #answer = (n) * (n-1) * (n-2) * (n-3) * (n-4) * (n-5) ...
 
-  #iterative
-  def self.calculate_factorial_iterative n
-    #TODO: Implement factorial using a loop
-    n
+  #iterative: 
+  #TODO: Implement factorial using a loop
+
+  def self.calculate_factorial_iterative(n)
+    # fac = 0
+
+    # n.times do |i|
+    # fac += n * (n-(i+1))
+    # end
+    # return fac
+
+    # n.times do |i|
+    # n *= (n-(i+1))
+
+    # i = n
+    # fac = n  
+    # while i > 0 do
+    #   fac = fac * (n -= 1)
+    #   i -= 1
+    # end
+    # return fac
+
+    # i = n
+    # fac = 1 
+    # while i > 0 do
+    #   fac = fac * n
+    #   n -= 1
+    # end
+    # return fac
+
+    if n == 0 
+      1
+    else
+      factorial = 1
+      n.times do |x|
+        factorial *= (n - x)
+      end
+      return factorial
+    end
   end
 
   #recursive
-  def self.calculate_factorial_recursive n
-    #TODO: Implement factorial using recursion
-    n
+
+  def self.calculate_factorial_recursive(n)
+    if n == 0 
+      1
+    else
+      n * self.calculate_factorial_recursive(n-1)
+    end
   end
 
 
--- lab/lib/traineegrocerybagger.rb
+++ lab/lib/traineegrocerybagger.rb
@@ -3,14 +3,33 @@ module TraineeGroceryBagger
   MAX_BAG_WEIGHT = 10
 
   def self.pack items
-    bags = nil
+    bags = nil if items == nil 
+    bags = nil if items == [] 
+   	
+   	while items.length > 0 do
+	   	tot_wt = 0 
+	    while tot_wt <= 10 do |i|
+	    	# choose the next item, and put it in the bag if it 
+	    	# doesn't make the bag total weight more than 10
+	    	# else start a new bag
+	    	if tot_wt + item[i] 
+
+
+	    	else
+	    end 
+	end
+
+
+    if bag[i] = 
+
+
 
     #TODO: Put all the items in bags with no bag weighing more than MAX_BAG_WEIGHT
     #Return the Bags in an array
     #Return each bag as an array of items
     #Note: there's no need to balance or optimize the bagging
 
-    bags
+    return bags
   end
 
 end
