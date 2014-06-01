8710ccee306f9e9a68ccc9f0792f33130f2ebb2d
Randy Carlisle <cisolutionsinc@gmail.com>
2014-06-01 13:10:53 -0700

    test are all passing

--- lab/lib/mathexpert.rb
+++ lab/lib/mathexpert.rb
@@ -5,17 +5,36 @@ module MathExpert
   #5! = 5 *  4 *  3 *  2 *  1 = 120
   #answer = (n) * (n-1) * (n-2) * (n-3) * (n-4) * (n-5) ...
 
-  #iterative
+  # #iterative
+  # def self.calculate_factorial_iterative n
+  #   #TODO: Implement factorial using a loop
+  #   n
+  # end
+
+  # #recursive
+  # def self.calculate_factorial_recursive n
+  #   #TODO: Implement factorial using recursion
+  #   n
+  # end
+
+#iterative
   def self.calculate_factorial_iterative n
-    #TODO: Implement factorial using a loop
-    n
+
+    result  = 1
+
+    for x in (2..n)
+      result *= x
+     end
+
+    result
   end
 
   #recursive
   def self.calculate_factorial_recursive n
-    #TODO: Implement factorial using recursion
-    n
+    if n > 1
+      n * calculate_factorial_recursive( n - 1 )
+    else
+      n
+    end
   end
-
-
 end
