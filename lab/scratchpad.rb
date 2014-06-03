93e06f6f4736e9913ce6f239ae9d82a658a8ae44
antmachine <anthonysallows@gmail.com>
2014-06-03 15:44:36 -0700

    added scratchpad

---
+++ lab/scratchpad.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+
+
+module TraineeGroceryBagger
+
+  MAX_BAG_WEIGHT = 10
+  i = 0
+  def self.pack items
+    all_bags = []
+    #this loop sums up the weight in a single bag of the all_bags array
+    	if single_bag weight > MAX_BAG_WEIGHT
+    		while bag_sum < MAX_BAG_WEIGHT do
+					bag_sum += all_bags[n][i]
+					i += 1
+				end
+			else
+				#add new bag and put items in it
+
+		#single item in a bag
+		all_bags[n][i]
+
+    #TODO: Put all the items in bags with no bag weighing more than MAX_BAG_WEIGHT
+    #Return the Bags in an array
+    #Return each bag as an array of items
+    #Note: there's no need to balance or optimize the bagging
+    
+    #TODO sum each bag individually and make sure < MAX_BAG_WEIGHT
+    #TODO if bag + next item > MAX_BAG_WEIGHT, move to next bag
+    #TODO return array of bags. each bag is an array of numbers
+
+
+
+
+    bags
+  end
+
+end
